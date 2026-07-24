991e1f8ce5b3abdf636f980551e14617af6a9689
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-24 19:13:34 +0530

    Data validation completed

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
-from Linear_regression_01.components.data_ingestion import DataIngestion
-from Linear_regression_01.config.configuration import ConfigurationManager
-from Linear_regression_01.components.data_validatation import DataValidation
 from Linear_regression_01.pipeline.stage_01_dataingestion import DataIngestionTrainingPipeline
 from Linear_regression_01.pipeline.stage_02_datavalidation import DataValidationTrainingPipeline
 from Linear_regression_01.logging import logger
 
+
 STAGE_NAME = "Data Ingestion Stage"
+
 try:
-    logger.info(f">>>>>> Stage {STAGE_NAME} Started <<<<<<<<<<")
-    data_ingestion = DataIngestionTrainingPipeline()
-    data_ingestion.main()
-    logger.info(f">>>>>>>>> Stage {STAGE_NAME} Completed <<<<<<<<<<")
+    logger.info(f">>>>>> Stage {STAGE_NAME} Started <<<<<<")
+
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+
+    logger.info(f">>>>>> Stage {STAGE_NAME} Completed <<<<<<")
 
 except Exception as e:
     logger.exception(e)
     raise e
 
-STAGE_NAME="DATA VALIDATION STAGE"
 
-class DataValidationTrainingPipeline:
+STAGE_NAME = "Data Validation Stage"
 
-    def main(self):
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} Started <<<<<<")
 
-        config = ConfigurationManager()
+    obj = DataValidationTrainingPipeline()
+    obj.main()
 
-        validation_config = config.get_data_validation_config()
+    logger.info(f">>>>>> Stage {STAGE_NAME} Completed <<<<<<")
 
-        validation = DataValidation(validation_config)
-
-        validation.validate_dataset()
-
-obj = DataValidationTrainingPipeline()
-
-obj.main() 
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/Linear_regression_01/pipeline/stage_02_datavalidation.py
+++ src/Linear_regression_01/pipeline/stage_02_datavalidation.py
@@ -1,6 +1,5 @@
 from Linear_regression_01.config.configuration import ConfigurationManager
 from Linear_regression_01.components.data_validatation import DataValidation
-from Linear_regression_01.logging import logger
 
 STAGE_NAME="DATA VALIDATION STAGE"
 
@@ -15,7 +14,3 @@ class DataValidationTrainingPipeline:
         validation = DataValidation(validation_config)
 
         validation.validate_dataset()
-
-obj = DataValidationTrainingPipeline()
-
-obj.main()        
